e42010b59d362732e71220ca09b6d04558304450
iNem0o <cuneaz.leo@gmail.com>
2013-12-02 18:47:47 +0100

    update atoum

--- tests/.atoum.php
+++ tests/.atoum.php
@@ -10,14 +10,14 @@ $cli->addWriter($stdOutWriter);
 /*
  * Xunit report
  */
-$xunitWriter = new \mageekguy\atoum\writers\file(__DIR__.'/atoum.xml');
+$xunitWriter = new \mageekguy\atoum\writers\file(__DIR__.'/code-coverage/atoum.xml');
 $xunit = new \mageekguy\atoum\reports\asynchronous\xunit();
 $xunit->addWriter($xunitWriter);
 
 /*
  * Clover xml coverage
  */
-$cloverWriter = new \mageekguy\atoum\writers\file(__DIR__.'/coverage.xml');
+$cloverWriter = new \mageekguy\atoum\writers\file(__DIR__.'/code-coverage/coverage.xml');
 $clover = new \mageekguy\atoum\reports\asynchronous\clover();
 $clover->addWriter($cloverWriter);
 
