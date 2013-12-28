675c8efacbda616b43b6a9b91e2bf70240313a76
iNem0o <cuneaz.leo@gmail.com>
2013-12-28 10:04:44 +0100

    Moving to Atoum installer

--- tests/launcher.php
+++ tests/launcher.php
@@ -3,10 +3,8 @@
     $bootstrapPath = __DIR__.'/scripts/bootstrap.php';
     $unitsFolderPath = __DIR__.'/units';
 
-    if(!file_exists($atoumPath))
-        file_put_contents($atoumPath,
-            file_get_contents("http://downloads.atoum.org/nightly/mageekguy.atoum.phar")
-        );
-
+    if(!file_exists($atoumPath)) {
+        print `curl -s https://raw.github.com/atoum/atoum-installer/master/installer | php -- --phar`;
+    }
 
     print `php $atoumPath -bf $bootstrapPath -d $unitsFolderPath`;
