fd166e928b075b3e42f2956557134ca8dc69c665
iNem0o <cuneaz.leo@gmail.com>
2013-12-28 10:56:44 +0100

    truncate table after test

--- tests/scripts/after_tests.sql
+++ tests/scripts/after_tests.sql
@@ -1,4 +1,4 @@
-DROP TABLE brands;
-DROP TABLE cars;
-DROP TABLE car_have_tag;
-DROP TABLE tags;
+TRUNCATE TABLE brands;
+TRUNCATE TABLE cars;
+TRUNCATE TABLE car_have_tag;
+TRUNCATE TABLE tags;
--- tests/scripts/before_tests.sql
+++ tests/scripts/before_tests.sql
@@ -1,11 +1,11 @@
-CREATE TABLE `brands` (
+CREATE TABLE IF NOT EXISTS `brands` (
   `idBrand` int(11) NOT NULL AUTO_INCREMENT,
   `nameBrand` varchar(100) NOT NULL,
   `noteBrand` float DEFAULT '0',
   PRIMARY KEY (`idBrand`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-CREATE TABLE `cars` (
+CREATE TABLE IF NOT EXISTS `cars` (
   `idCar` int(11) NOT NULL AUTO_INCREMENT,
   `idBrand` int(11) NOT NULL,
   `nameCar` varchar(100) NOT NULL,
@@ -13,13 +13,13 @@ CREATE TABLE `cars` (
   PRIMARY KEY (`idCar`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-CREATE TABLE `car_have_tag` (
+CREATE TABLE IF NOT EXISTS `car_have_tag` (
   `idCar` int(11) NOT NULL,
   `idTag` int(11) NOT NULL,
   PRIMARY KEY (`idCar`,`idTag`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-CREATE TABLE `tags` (
+CREATE TABLE IF NOT EXISTS `tags` (
   `idTag` int(11) NOT NULL AUTO_INCREMENT,
   `libTag` varchar(255) NOT NULL,
   PRIMARY KEY (`idTag`)
